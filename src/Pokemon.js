f454067c571f95ecdf6d88b2254d81287f8c928f
Quentin Ikeno <ikenoq@gmail.com>
2022-06-20 16:01:27 -1000

    Add Pokemon component.

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -2,10 +2,19 @@ import React from "react";
 
 function Pokemon({ id, name, type, base_experience }) {
 	return (
-		<div>
-			<h4>{name}</h4>
-			<p>Type: {type}</p>
-			<p>EXP: {base_experience}</p>
+		<div className="column is-3 has-background-light">
+			<h4 className="title is-4">{name}</h4>
+			<div className="card-image ">
+				<figure className="image is-128x128 mx-auto">
+					<img
+						src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+					/>
+				</figure>
+			</div>
+			<div className="card-content">
+				<p>Type: {type}</p>
+				<p>EXP: {base_experience}</p>
+			</div>
 		</div>
 	);
 }
