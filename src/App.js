bb380dead5ddede8dddab4014f4cae9bce09ddf2
Quentin Ikeno <ikenoq@gmail.com>
2022-06-20 14:27:00 -1000

    Add Pokedex component to App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Pokedex from "./Pokedex.js";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			hello
+			<Pokedex />
+		</div>
+	);
+}
+
+export default App;
